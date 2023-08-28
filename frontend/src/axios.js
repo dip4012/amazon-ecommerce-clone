499f2c2ad36adeccd76c3fe56a36e45e3005ea25
Dibyendu Saha <54032289+dip4012@users.noreply.github.com>
2023-08-28 20:38:22 +0530

    Update axios.js baseUrl

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://amazon-clone4012.herokuapp.com/",
+	baseURL: "https://amazon-clone-backend-zsn5.onrender.com/",
 });
 
 export default instance;
